baa27bbc4e4f85792865e43a4da4d0432d187de5
Seth Lafollette <sethlafollette@t-Seth-MacBook-Pro.local>
2020-04-30 15:49:31 -0400

    styling

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
 
-// $(":taget").on("click", function(){
-//     let offset = $(':target').offset();
-//     let scrollto = offset.top - 100; // minus fixed header height
-//     $('html, body').animate({scrollTop:scrollto}, 0);
-// });
