73943a9663b11859ffcb72d72b275fd95ce7cef1
Seth Lafollette <sethlafollette@t-Seth-MacBook-Pro.local>
2020-04-30 14:28:53 -0400

    fixed scrolling

---
+++ app.js
@@ -0,0 +1,6 @@
+
+// $(":taget").on("click", function(){
+//     let offset = $(':target').offset();
+//     let scrollto = offset.top - 100; // minus fixed header height
+//     $('html, body').animate({scrollTop:scrollto}, 0);
+// });
